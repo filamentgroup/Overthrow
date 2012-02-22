610804d946f128c6c5a27dd2a834232ca6658997
scottjehl <scott@scottjehl.com>
2012-02-22 21:36:22 +0700

    URL updates

--- overthrow.js
+++ overthrow.js
@@ -1,4 +1,4 @@
-/*! Overthrow v.0.1.0. An overflow:auto polyfill for responsive design. (c) 2012: Scott Jehl, Filament Group, Inc. https://filamentgroup.github.com/overthrow/license.txt */
+/*! Overthrow v.0.1.0. An overflow:auto polyfill for responsive design. (c) 2012: Scott Jehl, Filament Group, Inc. http://filamentgroup.github.com/Overthrow/license.txt */
 (function( w, undefined ){
 	
 	var doc = w.document,

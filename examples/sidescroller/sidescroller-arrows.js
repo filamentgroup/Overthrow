2fc03df8d1136781c2f8200bd8db04ade6be7f83
Scott Jehl <scott@scottjehl.com>
2013-08-01 10:46:45 -0500

    cleaned up the sidescroller prototype js so that it can receive passed config. Will need an organizational refactor once behavior is finalized

--- examples/sidescroller/sidescroller-arrows.js
+++ examples/sidescroller/sidescroller-arrows.js
@@ -1,76 +1,90 @@
-(function( w ){
-	if( !"querySelector" in w.document ){
+/*
+	overthrow sidescroller extension
+*/
+
+(function( w, o ){
+
+
+	if( !o || !"querySelector" in w.document ){
 		return;
 	}
 
-	var scrolls = w.document.querySelectorAll( ".overthrow-enabled .sidescroll-nextprev" );
+	o.sidescroller = function( elems, options ){
 
-	for( var i = 0; i < scrolls.length; i++ ){
+		var scrolls = elems,
+			roundedScroll = options && options.roundedScroll;
 
-		var thisScroll = scrolls[ i ].querySelector( ".overthrow" ),
-			nextPrev = w.document.createElement( "div" ),
-			handled;
+		for( var i = 0; i < scrolls.length; i++ ){
 
-		scrolls[ i ].setAttribute( "tabindex", "0" );
+			var thisScroll = scrolls[ i ].querySelector( ".overthrow" ),
+				nextPrev = w.document.createElement( "div" ),
+				handled;
 
-		nextPrev.className = "sidescroll-nextprev-links";
+			scrolls[ i ].setAttribute( "tabindex", "0" );
 
-		nextPrev.innerHTML = "<a href='#' class='sidescroll-prev'>Previous</a><a href='#' class='sidescroll-next'>Next</a>";
+			nextPrev.className = "sidescroll-nextprev-links";
 
-		function handleClick( e ){
-			e.preventDefault();
-			if( e.type === "keydown" || !handled ){
-				handled = true;
-				
+			nextPrev.innerHTML = "<a href='#' class='sidescroll-prev'>Previous</a><a href='#' class='sidescroll-next'>Next</a>";
+
+			function handleClick( e ){
+				e.preventDefault();
+				if( e.type === "keydown" || !handled ){
+					handled = true;
+					o.intercept();
+					var slideWidth = thisScroll.querySelector( "li" ).offsetWidth,
+						currScroll = thisScroll.scrollLeft,
+						slideNum = Math.round( currScroll / slideWidth ),
+						next = (e.type !== "keydown" && e.target.className.indexOf( "next" ) > -1) || e.keyCode === 39,
+						newSlide = slideNum + ( next ? 1 : -1 ),
+						newScroll = slideWidth * newSlide;
+
+					o.toss( thisScroll, { left: newScroll, duration: 60 } );
+					setTimeout( function(){ handled = false; }, 100 );
+				}
+
+			}
+
+			function handleSnap(){
+				o.intercept();
 				var slideWidth = thisScroll.querySelector( "li" ).offsetWidth,
-					currScroll = thisScroll.scrollLeft,
-					slideNum = Math.round( currScroll / slideWidth ),
-					next = (e.type !== "keydown" && e.target.className.indexOf( "next" ) > -1) || e.keyCode === 39,
-					newSlide = slideNum + ( next ? 1 : -1 ),
-					newScroll = slideWidth * newSlide;
+						currScroll = thisScroll.scrollLeft,
+						slideNum = Math.round( currScroll / slideWidth );
 
-				overthrow.toss( thisScroll, { left: newScroll, duration: 80 } );
-				setTimeout( function(){ handled = false; }, 100 );
+				o.toss( thisScroll, { left: slideWidth * slideNum, duration: 20 } );
 			}
 
-		}
-
-		function handleSnap(){
-			var slideWidth = thisScroll.querySelector( "li" ).offsetWidth,
-					currScroll = thisScroll.scrollLeft,
-					slideNum = Math.round( currScroll / slideWidth );
-
-			overthrow.toss( thisScroll, { left: slideWidth * slideNum, duration: 20 } );
-		}
-
-		var debounce;
-		function handleResize(){
-			clearTimeout(debounce);
-			debounce = setTimeout(handleSnap, 100);
-		}
-
-		var debouncedos;
-		function handleScroll(){
-			clearTimeout(debouncedos);
-			debouncedos = setTimeout(handleSnap, 200);
-		}
-
-		function handleKey( e ){			
-			if( e.keyCode === 39 || e.keyCode === 37 ){
-				handleClick( e );
+			var debounce;
+			function handleResize(){
+				clearTimeout(debounce);
+				debounce = setTimeout(handleSnap, 100);
 			}
+
+			var debouncedos;
+			function handleScroll(){
+				clearTimeout(debouncedos);
+				debouncedos = setTimeout(handleSnap, 200);
+			}
+
+			function handleKey( e ){			
+				if( e.keyCode === 39 || e.keyCode === 37 ){
+					handleClick( e );
+				}
+			}
+
+
+			nextPrev.addEventListener( "click", handleClick, false );
+			nextPrev.addEventListener( "touchend", handleClick, false );
+			w.addEventListener( "resize", handleResize, false );
+			scrolls[ i ].addEventListener( "keydown", handleKey, false );
+			if( roundedScroll ){
+				thisScroll.addEventListener( "scroll", handleScroll, false );
+			}
+	 
+			scrolls[ i ].insertBefore( nextPrev, thisScroll );
+
+
 		}
 
+	};
 
-		nextPrev.addEventListener( "click", handleClick, false );
-		nextPrev.addEventListener( "touchend", handleClick, false );
-		w.addEventListener( "resize", handleResize, false );
-		scrolls[ i ].addEventListener( "keydown", handleKey, false );
-		thisScroll.addEventListener( "scroll", handleScroll, false );
- 
-		scrolls[ i ].insertBefore( nextPrev, thisScroll )
-
-
-	}
-
-}( this ));
+}( this, this.overthrow ));

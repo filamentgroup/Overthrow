e0a98325baf3a088da76c3446a49fa0d9be345eb
Scott Jehl <scott@scottjehl.com>
2013-08-07 18:14:13 -0500

    Sidescroller change: This is a first pass at emitting events for next and previous slide changes. With this change, events for overthrow-next and overthrow-prev will be triggered on the overthrow sidescroller whenever slides are advanced to the right or left, respectively. The event object of these events contains an overthrow object with properties such as active (which is an array of currently visible slides), and originalEvent (which is the event object that triggered the change - scroll, keydown, click, resize). Lastly, the sidescrollerElement.getActiveSlides() method is exposed on each sidescroller overthrow  and returns the active slides at any time when called. This addresses #42

--- extensions/overthrow-sidescroller.js
+++ extensions/overthrow-sidescroller.js
@@ -11,9 +11,23 @@
 		return;
 	}
 
+	function sendEvent( elem, evt, args ){
+		// TODO needs IE8 support
+		if( document.createEvent ){
+			var event = document.createEvent( "Event" );
+			event.initEvent( evt, true, true );
+			event.overthrow = args;
+			elem.dispatchEvent( event );
+		}
+	}
+
 	o.sidescroller = function( elems, options ){
 
 		var scrolls = elems,
+			evtPrefix = "overthrow",
+			evtNext = evtPrefix + "-next",
+			evtPrev = evtPrefix + "-prev",
+			slideNum = 0,
 			snapScroll = options && options.snapScroll,
 			rewind = options && options.rewind;
 
@@ -29,24 +43,53 @@
 
 			nextPrev.innerHTML = "<a href='#' class='sidescroll-prev'>Previous</a><a href='#' class='sidescroll-next'>Next</a>";
 
+			function getActiveSlides( left ){
+				var slides = thisScroll.querySelectorAll( "li" ),
+					numSlides = slides.length,
+					slidesWidth = thisScroll.offsetWidth,
+					slideWidth = slides[ 0 ].offsetWidth,
+					scrollLeft = left !== undefined ? left : thisScroll.scrollLeft,
+					startSlide = Math.round( scrollLeft / slideWidth ),
+					valid = true,
+					ret = [];
+
+				startSlide = Math.max( 0, startSlide );
+				startSlide = Math.min( numSlides, startSlide );
+
+				ret[ 0 ] = startSlide;
+				for( var i = 1; i < numSlides; i++ ){
+					if( startSlide + (i * slideWidth) < slidesWidth){
+						ret.push( startSlide + i);
+					}
+					else{
+						valid = false;
+					}
+				}
+				slideNum = startSlide;
+				return ret;
+			}
+
+			// expose the getactiveslides function on the overthrow element
+			scrolls[ i ].getActiveSlides = getActiveSlides;
+
 			function handleClick( e ){
 				e.preventDefault();
 				if( e.type === "keydown" || !handled ){
 					handled = true;
 					o.intercept();
 					var slides = thisScroll.querySelectorAll( "li" ),
+						slidesWidth = thisScroll.offsetWidth,
 						slideWidth = slides[ 0 ].offsetWidth,
 						currScroll = thisScroll.scrollLeft,
 						slideNum = Math.round( currScroll / slideWidth ),
 						next = (e.type !== "keydown" && e.target.className.indexOf( "next" ) > -1) || e.keyCode === 39,
 						newSlide = slideNum + ( next ? 1 : -1 ),
-						newScroll = slideWidth * newSlide;
+						newScroll = slideWidth * newSlide,
+						scrollWidth = thisScroll.scrollWidth - slidesWidth;
 
 					// if can't go left, go to end
 					if( rewind ){
 						
-						var scrollWidth = thisScroll.scrollWidth - thisScroll.offsetWidth;
-
 						if( newScroll < 0 ){
 							newScroll = scrollWidth;
 						}
@@ -54,32 +97,72 @@
 							newScroll = 0;
 						}
 					}
+					else {
+						if( newScroll < 0 ){
+							newScroll = 0;
+						}
+						else if( newScroll > scrollWidth ){
+							newScroll = scrollWidth;
+						}
+					}
 
-					o.toss( thisScroll, { left: newScroll } );
+					var newActive = getActiveSlides( newScroll );
+
+					if( newActive[ 0 ] !== slideNum ){
+
+						o.toss( thisScroll, { left: newScroll } );
+
+						sendEvent(
+							thisScroll, // elem to receive event
+							next ? evtNext : evtPrev, // evt name
+							{
+								active: newActive, // active slides
+								originalEvent: e
+							}
+						);
+					}
+
 					setTimeout( function(){ handled = false; }, 100 );
 				}
 
 			}
 
-			function handleSnap(){
+			function handleSnap( e ){
 				o.intercept();
 				var slideWidth = thisScroll.querySelector( "li" ).offsetWidth,
-						currScroll = thisScroll.scrollLeft,
-						slideNum = Math.round( currScroll / slideWidth );
+					currScroll = thisScroll.scrollLeft,
+					newSlide = Math.round( currScroll / slideWidth ),
+					newScroll = slideWidth * newSlide;
 
-				o.toss( thisScroll, { left: slideWidth * slideNum, duration: 20 } );
+				o.toss( thisScroll, { left: newScroll, duration: 20 } );
+
+				if( slideNum !== newSlide ){
+					sendEvent(
+						thisScroll, // elem to receive event
+						newSlide > slideNum ? evtNext : evtPrev, // evt name
+						{
+							active: getActiveSlides( newScroll ), // active slides
+							originalEvent: e
+						}
+					);
+					slideNum = newSlide;
+				}
 			}
 
 			var debounce;
-			function handleResize(){
+			function handleResize( e ){
 				clearTimeout(debounce);
-				debounce = setTimeout(handleSnap, 100);
+				debounce = setTimeout(function(){
+					handleSnap( e );
+				}, 100);
 			}
 
 			var debouncedos;
-			function handleScroll(){
+			function handleScroll( e ){
 				clearTimeout(debouncedos);
-				debouncedos = setTimeout(handleSnap, 200);
+				debouncedos = setTimeout(function(){
+					handleSnap( e );
+				}, 200);
 			}
 
 			function handleKey( e ){			

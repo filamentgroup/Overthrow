1550694f14252f15166bda54203084320f2e89ca
Jeffrey Lembeck <jlembeck@gmail.com>
2013-09-20 13:39:14 -0700

    Remove dupe + fix bug with math

--- extensions/overthrow-sidescroller.goto.js
+++ extensions/overthrow-sidescroller.goto.js
@@ -18,6 +18,23 @@
 		}
 	}
 
+	function determineNewSlide( num, currSlideNum ){
+		var newSlide = 0, relative = false, sign;
+
+		if( typeof num === "string" ){
+			sign = num.charAt( 0 );
+			if( sign === "+" || sign === "-" ){
+				relative = true;
+			}
+		}
+		if( relative ){
+			newSlide = parseInt( num, 10 ) + currSlideNum;
+		} else {
+			newSlide = parseInt( num, 10 );
+		}
+		return newSlide;
+	}
+
 	function goTo( event ) {
 		var options = event.overthrow || {}, goto, scroller, ul, thisScroll;
 
@@ -30,13 +47,20 @@
 		thisScroll = scroller.querySelector( ".overthrow" );
 		ul = scroller.querySelector( "ul" );
 
-		console.log( scroller );
 		var slides = ul.querySelectorAll( "li" ),
-			slidesWidth = scroller.offsetWidth,
+			slidesWidth = thisScroll.offsetWidth,
 			slideWidth = slides[ 0 ].offsetWidth,
-			newSlide = parseInt( goto, 10),
-			newScroll = slideWidth * newSlide;
+			currScroll = thisScroll.scrollLeft,
+			currSlideNum = Math.round( currScroll / slideWidth ),
+			newSlide = determineNewSlide( goto, currSlideNum ),
+			newScroll = slideWidth * newSlide,
+			scrollWidth = thisScroll.scrollWidth - slidesWidth;
 
+			if( newScroll < 0 ){
+				newScroll = 0;
+			} else if( newScroll > scrollWidth ){
+				newScroll = scrollWidth;
+			}
 			var newActive = scroller.getActiveSlides( newScroll );
 
 			overthrow.toss( thisScroll, {
@@ -44,6 +68,10 @@
 				easing: options.easing
 			});
 
+			sendEvent( scroller,
+								"goto.overthrow",
+								goto,
+								thisScroll.ieID );
 	}
 
 	lib.onEvent( "overthrow-init", w.document.documentElement, function( event ) {

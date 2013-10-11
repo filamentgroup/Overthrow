798d686549c1b974c644a12d965dee1cd30588df
John Bender <john.m.bender@gmail.com>
2013-10-11 11:58:37 -0700

    disabled nav tests

--- test/tests-sidescroller.js
+++ test/tests-sidescroller.js
@@ -4,24 +4,49 @@
 
 window.onload = function(){
 
-	var testElems = document.querySelectorAll( "#testelem" );
+	var scroller, testElem;
 
 	module( "properties", {
 		setup: function(){
-			overthrow.sidescroller( testElems );
+      testElem = document.querySelector( "#testelem" );
+			scroller = testElem.querySelector( ".overthrow.sidescroll" );
+			scroller.scrollLeft = 0;
+			overthrow.sidescroller( [testElem], {fixedItemWidth: true} );
 		}
 	});
 
-	/* TESTS HERE */
-	test( 'API Properties: overthrow is defined', function() {
-		ok( overthrow.sidescroller );
+	function nextDisabled(){
+		return testElem.querySelectorAll(".sidescroll-next.disabled");
+	}
+
+	function prevDisabled(){
+		return testElem.querySelectorAll(".sidescroll-prev.disabled");
+	}
+
+	test( "only nav prev should be disabled initially", function() {
+		var next, prev;
+
+		prev = prevDisabled();
+		equal( prev.length, 1 );
+
+		next = nextDisabled();
+		equal( next.length, 0 );
 	});
 
-	test( "nav prev should be disabled initially", function() {
-		//
-	});
+	asyncTest( "nav next should be disabled after scroll", function() {
+		var next;
 
-	test( "nav next should be disabled after scroll", function() {
-		//
+		next = nextDisabled();
+		equal( next.length, 0 );
+
+		console.log( scroller );
+		scroller.scrollLeft = 10000;
+
+		setTimeout(function() {
+			next = nextDisabled();
+
+			equal( next.length, 1 );
+			start();
+		}, 500);
 	});
 };

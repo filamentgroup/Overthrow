d4c761be2efc5450fbd0fe25ea7fbf49b1a7ef87
John Bender <john@filamentgroup.com>
2013-10-11 10:37:01 -0700

    start on tests

--- test/tests-sidescroller.js
+++ test/tests-sidescroller.js
@@ -1,24 +1,27 @@
 /*
-Overthrow unit tests - using qUnit
-*/
+ Overthrow unit tests - using qUnit
+ */
 
 window.onload = function(){
-	
-	var testElem = document.getElementById( "testelem" );
 
-	
+	var testElems = document.querySelectorAll( "#testelem" );
 
-	overthrow.sidescroller( testElem  );
+	module( "properties", {
+		setup: function(){
+			overthrow.sidescroller( testElems );
+		}
+	});
 
-	
-		
-	
-		
-		/* TESTS HERE */ 
-		test( 'API Properties: overthrow is defined', function() { 
-			ok( overthrow.sidescroller );
-		});
-		
+	/* TESTS HERE */
+	test( 'API Properties: overthrow is defined', function() {
+		ok( overthrow.sidescroller );
+	});
 
-	
+	test( "nav prev should be disabled initially", function() {
+		//
+	});
+
+	test( "nav next should be disabled after scroll", function() {
+		//
+	});
 };

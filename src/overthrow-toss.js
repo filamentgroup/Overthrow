8b3a09d76d327ff088763b429587898e3f8bb710
Scott Jehl <scottjehl@gmail.com>
2016-07-21 13:38:26 -0500

    fix demo link

--- src/overthrow-toss.js
+++ src/overthrow-toss.js
@@ -1,25 +1,25 @@
 /*! Overthrow. An overflow:auto polyfill for responsive design. (c) 2012: Scott Jehl, Filament Group, Inc. http://filamentgroup.github.com/Overthrow/license.txt */
 /* overthrow.toss simply retains the overthrow.toss api by pulling in the external toss dependency and borring its methods */
-(function( w, o, t, undefined ){
+(function( w, t, undefined ){
 
 	// t is a reference to the external toss dependency
 	if( t === undefined ){
 		return;
 	}
 
-	// o is overthrow reference from overthrow-polyfill.js
-	if( o === undefined ){
-		w.overthrow = o = {};
+	// w.overthrow is overthrow reference from overthrow-polyfill.js
+	if( w.overthrow === undefined ){
+		w.overthrow = {};
 	}
 
-	o.toss = t;
-	o.easing = t.easing;
-	o.tossing = t.tossing;
-	o.intercept = function( elem ){
+	w.overthrow.toss = t;
+	w.overthrow.easing = t.easing;
+	w.overthrow.tossing = t.tossing;
+	w.overthrow.intercept = function( elem ){
 			if( !elem ){
 				return;
 			}
-			o.tossing( elem, false );
+			w.overthrow.tossing( elem, false );
 		};
 
-})( this, this.overthrow, this.toss );
+})( this, this.toss );
